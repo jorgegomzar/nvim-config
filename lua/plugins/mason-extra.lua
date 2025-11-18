01a1c1961e1382d2638de9760da1c68858cae123
Jorge Gómez Zarzosa <jorgegomez@bmat.com>
2025-11-18 11:12:58 +0100

    feat: add mason extra config

---
+++ lua/plugins/mason-extra.lua
@@ -0,0 +1,21 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "groovy-language-server",
+      "lua-language-server",
+      "markdown-oxide",
+      "mypy",
+      "npm-groovy-lint",
+      "prettier",
+      "pyproject-fmt",
+      "pyright",
+      "python-lsp-server",
+      "ruff",
+      "shfmt",
+      "stylua",
+      "tombi",
+      "yaml-language-server",
+    },
+  },
+}
